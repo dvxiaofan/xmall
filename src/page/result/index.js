58cda6f5368de92945b2b2f54d6f216e2832c649
xiaofan <1353815633@qq.com>
2018-11-13 16:57:38 +0800

    配置页面title

---
+++ src/page/result/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: xiaofan 
+ * @Date: 2018-11-13 16:38:15 
+ * @Last Modified by: xiaofan
+ * @Last Modified time: 2018-11-13 16:38:38
+ */
+
+ require('./index.css');
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: xiaofan 
  * @Date: 2018-11-08 11:32:11 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-11-13 16:04:12
+ * @Last Modified time: 2018-11-13 16:47:54
  */
 
 var webpack = require('webpack');
@@ -12,14 +12,15 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 // 配置环境变量 dev / online 
 var WEBPACK_ENV = process.env.WEBPACK_ENV || '';
 
-// 或许HTML的webpack参数
-var getHtmlWebpackPlugin = function (name) {
+// 获取HTML的webpack参数
+var getHtmlWebpackPlugin = function (name, title) {
   return {
-    template: './src/view/' + name + '.html',
-    filename: 'view/' + name + '.html',
-    inject: true,
-    hash: true,
-    chunks: ['common', name]
+    template  : './src/view/' + name + '.html',
+    filename   : 'view/' + name + '.html',
+    inject    : true,
+    title     : title,
+    hash      : true,
+    chunks    : ['common', name]
   };
 };
 
@@ -28,6 +29,7 @@ var config = {
     'index': ['./src/page/index/index.js'],
     'login': ['./src/page/login/index.js'],
     'common': ['./src/page/common/index.js'],
+    'result': ['./src/page/result/index.js'],
   },
   output: {
     path: './dist', // 打包路径
@@ -71,14 +73,15 @@ var config = {
     // 单独打包处理css
     new Ex('css/[name].css'),
     // html处理
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('index')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('login')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('register')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('list')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('order-list')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('user-center')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('about')),
-    new HtmlWebpackPlugin(getHtmlWebpackPlugin('cart')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('index', '首页')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('login', '用户登录')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('register', '用户注册')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('result', '操作结果')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('list', '结果列表')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('order-list', '订单中心')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('user-center', '个人中心')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('about', '关于MMall')),
+    new HtmlWebpackPlugin(getHtmlWebpackPlugin('cart', '购物车')),
   ],
 };
 
