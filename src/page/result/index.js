c2ec3a82fc54aa49b19dad23f3c0d5bf8e7ff808
xiaofan <1353815633@qq.com>
2018-11-13 17:24:43 +0800

    构建操作结果提示页面

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -2,7 +2,19 @@
  * @Author: xiaofan 
  * @Date: 2018-11-13 16:38:15 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-11-13 16:38:38
+ * @Last Modified time: 2018-11-13 17:23:05
  */
 
- require('./index.css');
+require('./index.css');
+require('../common/nav-simple/index.js');
+
+const _mm = require('util/mm.js');
+
+$(() => {
+	const type = _mm.getUrlParam('type') || 'default';
+	const $element = $(`.${type}-success`);
+
+	// 现实结果提示
+	$element.show();
+	
+});
