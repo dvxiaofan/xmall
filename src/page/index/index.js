f6f0bc543d1d54bd3a605c16fe6eeda28a26856a
xiaofan <1353815633@qq.com>
2018-11-19 12:13:50 +0800

    构建首页banner

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,18 +2,31 @@
  * @Author: xiaofan 
  * @Date: 2018-11-08 15:42:22 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-11-16 16:45:03
+ * @Last Modified time: 2018-11-19 12:10:45
  */
 
 require('./index.css');
 require('../common/nav-simple/index.js');
 require('../common/nav/index.js');
 require('../common/header/index.js');
+require('util/slider/index.js');
 
 const navSide = require('../common/nav-side/index.js');
-
+const temBanner = require('./banner.string');
 const _mm = require('util/mm.js');
 
-navSide.init({
-	name: 'about'
+
+$(function () {
+	const bnnerHtml = _mm.renderHtml(temBanner);
+	$('.banner-con').html(bnnerHtml);
+
+	// 初始化unslider
+	const $unslider = $('.banner').unslider({
+		dots: true,
+	});
+
+	$('.banner-con .banner-arrow').click(function() {
+		var fd = $(this).hasClass('prev') ? 'prev' :'next';
+		$unslider.data('unslider')[fd]();
+	});
 });
---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: xiaofan 
+ * @Date: 2018-11-18 19:20:29 
+ * @Last Modified by: xiaofan
+ * @Last Modified time: 2018-11-18 19:23:07
+ */
+
+
+require('./index.css');
+require('./unslider.js');
