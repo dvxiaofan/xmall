4b6864f3db5adfae5f0c63b91838205ca1a72223
xiaofan <1353815633@qq.com>
2018-11-23 00:15:01 +0800

    构建订单确认页面

--- src/page/order-confirm/index.js
+++ src/page/order-confirm/index.js
@@ -2,7 +2,7 @@
  * @Author: xiaofan 
  * @Date: 2018-11-21 12:06:09 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-11-21 23:48:29
+ * @Last Modified time: 2018-11-23 00:08:25
  */
 
 require('./index.css');
@@ -11,9 +11,10 @@ require('../common/nav/index.js');
 
 const _mm 						= require('util/mm.js');
 const _order 					= require('service/order-service.js');
-const _address					= require('service/address-service.js');
+const _address				= require('service/address-service.js');
 const tempAddress 		= require('./address-list.string');
 const tempProduct 		= require('./product-list.string');
+const addressModal 		= require('./address-modal.js');
 
 const page = {
 	data: {
@@ -41,8 +42,47 @@ const page = {
 
 		// 地址添加
 		$(document).on('click', '.address-add', function () {
-			console.log(111);
+			// 弹出添加地址模块
+			addressModal.show({
+				isUpdate: false,
+				onSuccess: () => {
+					_this.loadAddressList();
+				}
+			});
+		});
+
+		// 地址编辑
+		$(document).on('click', '.address-update', function (e) {
+			e.stopPropagation();
 			
+			const shippingId = $(this).parents('.address-item').data('id');
+			_address.getAddress(shippingId, (res) =>{
+				// 弹出添加地址模块
+				addressModal.show({
+					isUpdate: true,
+					data		: res,
+					onSuccess: () => {
+						_this.loadAddressList();
+					}
+				})
+			}, (errorMsg) => {
+				_mm.errorTips(errorMsg);
+			});
+		});
+
+		// 地址删除
+		$(document).on('click', '.address-delete', function (e) {
+			e.stopPropagation();
+			
+			const shippingId = $(this).parents('.address-item').data('id');
+			if(window.confirm('确定删除这条地址吗？')) {
+				_address.deleteAddress(shippingId, () => {
+					_mm.successTips('地址删除成功');
+					_this.loadAddressList();
+				}, (errorMsg) => {
+					_mm.errorTips(errorMsg);
+				})
+			}
 		});
 
 		// 订单提交
@@ -66,7 +106,9 @@ const page = {
 	loadAddressList() {
 		const _this = this,
 			$addressCon = $('.address-con');
+			$addressCon.html('<div class="loading"></div>');
 		_address.getAddressList((res) => {
+			_this.addressFilter(res);
 			var addressHtml = _mm.renderHtml(tempAddress, res);
 			$addressCon.html(addressHtml);
 		}, (errorMsg) => {
@@ -74,10 +116,28 @@ const page = {
 		});
 	},
 
+	// 地址列表选中的状态处理
+	addressFilter(data) {
+		if(this.data.seletedAddressId) {
+			var selectedAddressIdFlag = false;
+			for (let i = 0, iLength = data.list.length; i < iLength; i++) {
+				if(data.list[i].id === this.data.seletedAddressId) {
+					data.list[i].isActive = true;
+					selectedAddressIdFlag = true;
+				}
+			};
+			// 以前选中的不存在了,将其删除
+			if(!selectedAddressIdFlag) {
+				this.data.seletedAddressId = null;
+			}
+		}
+	},
+
 	// 加载商品列表
 	loadProductList() {
 		const _this = this,
 			$productCon = $('.product-con');
+			$productCon.html('<div class="loading"></div>');
 		_order.getProductList((res) => {
 			var productHtml = _mm.renderHtml(tempProduct, res);
 			$productCon.html(productHtml);
