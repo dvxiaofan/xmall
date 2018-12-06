4956611d400467c3b89eb24545ce2fab956d1170
xiaofan <1353815633@qq.com>
2018-12-06 17:02:33 +0800

    升级 webpack 到4.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,95 +2,121 @@
  * @Author: xiaofan 
  * @Date: 2018-11-08 11:32:11 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-03 17:09:34
+ * @Last Modified time: 2018-12-06 17:02:00
  */
 
-var webpack           = require('webpack');
-var Ex                = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var Ex = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // 配置环境变量 dev / online 
 var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
 
 // 获取HTML的webpack参数
-var getHtmlWebpackPlugin = function (name, title) {
+var getHtmlWebpackPlugin = (name, title) => {
   return {
-    template  : './src/view/' + name + '.html',
-    filename   : 'view/' + name + '.html',
-    favicon   : './favicon.ico',
-    inject    : true,
-    title     : title,
-    hash      : true,
-    chunks    : ['common', name]
+    template: './src/view/' + name + '.html',
+    filename: 'view/' + name + '.html',
+    favicon: './favicon.ico',
+    inject: true,
+    title: title,
+    hash: true,
+    chunks: ['common', name]
   };
 };
 
 var config = {
+  mode: 'dev' === WEBPACK_ENV ? 'development' : 'production',
   entry: {
-    'index'                 : ['./src/page/index/index.js'],
-    'list'                  : ['./src/page/list/index.js'],
-    'detail'                : ['./src/page/detail/index.js'],
-    'cart'                  : ['./src/page/cart/index.js'],
-    'order-confirm'          : ['./src/page/order-confirm/index.js'],
-    'order-list'            : ['./src/page/order-list/index.js'],
-    'order-detail'          : ['./src/page/order-detail/index.js'],
-    'payment'               : ['./src/page/payment/index.js'],
-    'user-login'            : ['./src/page/user-login/index.js'],
-    'user-register'         : ['./src/page/user-register/index.js'],
-    'user-pass-reset'       : ['./src/page/user-pass-reset/index.js'],
-    'user-pass-update'      : ['./src/page/user-pass-update/index.js'],
-    'user-center'           : ['./src/page/user-center/index.js'],
-    'user-center-update'    : ['./src/page/user-center-update/index.js'],
-    'common'                : ['./src/page/common/index.js'],
-    'result'                : ['./src/page/result/index.js'],
-    'about'                 : ['./src/page/about/index.js'],
+    'index': './src/page/index/index.js',
+    'list': './src/page/list/index.js',
+    'detail': './src/page/detail/index.js',
+    'cart': './src/page/cart/index.js',
+    'order-confirm': './src/page/order-confirm/index.js',
+    'order-list': './src/page/order-list/index.js',
+    'order-detail': './src/page/order-detail/index.js',
+    'payment': './src/page/payment/index.js',
+    'user-login': './src/page/user-login/index.js',
+    'user-register': './src/page/user-register/index.js',
+    'user-pass-reset': './src/page/user-pass-reset/index.js',
+    'user-pass-update': './src/page/user-pass-update/index.js',
+    'user-center': './src/page/user-center/index.js',
+    'user-center-update': './src/page/user-center-update/index.js',
+    'common': './src/page/common/index.js',
+    'result': './src/page/result/index.js',
+    'about': './src/page/about/index.js',
   },
   output: {
-    path      : __dirname + '/dist/', // 打包路径
     publicPath: 'dev' === WEBPACK_ENV ? '/dist/' : '//s.webxiaofan.com/xmall-fe/dist/', // 访问路径
-    filename   : 'js/[name].js',
+    filename: 'js/[name].js',
   },
-  externals : {
-    'jquery' : 'window.jQuery'
+  externals: {
+    'jquery': 'window.jQuery'
   },
   module: {
-    loaders: [{
-        test    : /\.css$/,
-        loader  : Ex.extract('style-loader', 'css-loader')
+    rules: [{
+        test: /\.css$/,
+        use: Ex.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
       }, {
-        test    : /\.string$/,
-        loader  : 'html-loader',
-        query   : {
-          minimize: true,
-          removeAttrivuteQuotes: false
-        },
-      },
-      {
-        test    : /\.(png|gif|jpg|svg|woff|eot|ttf)\??.*$/,
-        loader  : 'url-loader',
-        query   : {
-          limit     : 100,
-          // publicPath: '../', // 解决打包后路径不对的问题
-          name      : 'resource/[name].[ext]'
+        test: /\.string$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+            removeAttrivuteQuotes: false
+          }
         }
+      },
+      // 图片配置
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 2048,
+            name: 'resource/[name].[ext]'
+          }
+        }]
+      },
+      // 字体图标配置
+      {
+        test: /\.(svg|woff|woff2|eot|ttf)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'resource/[name].[ext]'
+          }
+        }]
       },
     ]
   },
   resolve: {
     alias: {
-      node_modules  : __dirname + '/node_modules',
-      util          : __dirname + '/src/util',
-      page          : __dirname + '/src/page',
-      service       : __dirname + '/src/service',
-      images        : __dirname + '/src/images',
+      node_modules: __dirname + '/node_modules',
+      util: __dirname + '/src/util',
+      page: __dirname + '/src/page',
+      service: __dirname + '/src/service',
+      images: __dirname + '/src/images',
     }
   },
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          name: 'common',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
+  },
+
   plugins: [
-    // 独立通用模块到js/base.js
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'js/base.js',
-    }),
     // 单独打包处理css
     new Ex('css/[name].css'),
     // html处理
@@ -111,10 +137,18 @@ var config = {
     new HtmlWebpackPlugin(getHtmlWebpackPlugin('about', '关于XMall')),
     new HtmlWebpackPlugin(getHtmlWebpackPlugin('cart', '购物车')),
   ],
+
+  devServer: {
+    port: 8088,
+    inline: true,
+    proxy: {
+      '**/*.do': {
+        target: 'http://test.happymmall.com',
+        changeOrigin: true
+      }
+    }
+  }
 };
 
-if ('dev' === WEBPACK_ENV) {
-  config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
-}
 
 module.exports = config;
