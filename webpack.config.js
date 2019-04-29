f97a7dc9250b461844a6476c8768d6e16a984f28
xiaofan <1353815633@qq.com>
2019-04-29 16:27:01 +0800

    添加文件头部信息

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+/*
+ * @Author: DevZhang 
+ * @Date: 2019-04-29 16:16:09 
+ * @Last Modified by: DevZhang
+ * @Last Modified time: 2019-04-29 16:18:06
+ */
 
-const path = './dist';
 
-module.exports = {
+let config = {
     entry: './src/page/index/index.js',
     output: {
-        path: path,
+        path: './dist/js',
         filename: 'app.js'
     }
 }
+
+module.exports = config;
